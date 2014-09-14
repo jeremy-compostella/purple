ae8a31813e15920e7fcae2fc1263d3102d233565
Jeremy Compostella <jeremy.compostella@gmail.com>
2014-09-14 20:01:55 +0200

    fix comment

--- purple-account.el
+++ purple-account.el
@@ -20,7 +20,7 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (defgroup purple-account nil
-  "Activity management group"
+  "Purple account group."
   :group 'purple)
 
 (defvar purple-accounts '())
